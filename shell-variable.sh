5080ac661964067d0dbd2036e5c53e527ea0a50a
niteshsharma99 <ns476280@gmail.com>
2024-06-25 04:25:43 +0000

    First Commit

---
+++ shell-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#variable vaules
+string1=10
+String1=20
+
+echo "The value of string 1 is $string1 "
+echo "The value od String 1 is $String1 "
+echo "The vaule of HOME env varibale is $HOME "
